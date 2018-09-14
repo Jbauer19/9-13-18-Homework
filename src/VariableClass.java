ecab6eeb20cce3ced573855283dc1a2ad029aba0
Jbauer19 <42978498+Jbauer19@users.noreply.github.com>
2018-09-14 13:54:34 -0400

    Add files via upload

---
+++ src/VariableClass.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ * @author Josh Bauer
+ * Variables 
+ * Period 7
+ *
+ */
+public class VariableClass 
+{
+
+	public static void main(String[] args) 
+	{
+		String name = "Josh";
+		System.out.println(name);
+		
+		int length = 5;
+		int width = 2;
+		int height = 10;
+		int rectangleArea = length*width*height;
+		System.out.println(rectangleArea);
+		
+		double pie = 3.14;
+		int radius = 10;
+		
+		int circleArea = (int) (radius*radius*pie);
+		System.out.println(circleArea);
+		
+		int circumphrence = (int) (2*pie*radius); 
+		System.out.println(circumphrence);
+	}
+
+
+	
+	
+}
